be0009c7b96d5a2217a1bc968e9d719090f8b4d1
Myle$ <mcutchember@fullsail.edu>
2019-05-12 10:31:54 -0400

    initial commit
    installed Alamofire for networking.

---
+++ iTunesClient/iTunesClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunesClient
+//
+//  Created by Myle$ on 5/12/19.
+//  Copyright © 2019 Myle$. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
